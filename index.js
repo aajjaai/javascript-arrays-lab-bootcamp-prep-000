db65f677ec478eafd60f64575f83eb637721c33e
ajay <ajay.kumar@unicodesystems.org>
2018-09-06 08:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add your functions and code here
+
+window.kittens = ['Milo', 'Otis', 'Garfield'];
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+
+
+destructivelyAppendKitten('Ralph', concat('kittens'))
+  
